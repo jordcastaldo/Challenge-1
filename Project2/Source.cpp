ac54944c0ba298e3553d70b1cf6960afce5f9862
jordf <jordf@DESKTOP-VP9JCUB>
2022-01-24 12:31:45 -0700

    Add project files.

---
+++ Project2/Source.cpp
@@ -0,0 +1,19 @@
+//The purpose of this program is to calculate the sum of 100 and 50
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//This line of code is to define variables
+	double hundred, fifty, total;
+	hundred = 100;
+	fifty = 50;
+
+	//This line is to find the sum of the variables hundred and fifty and put it in a vaiable called total
+	total = hundred + fifty;
+
+	//Displays the total of the sum of the two variabls
+	cout << "The sum of 50 and 100 is " << total;
+	return 0;
+
+}
